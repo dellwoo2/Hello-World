fde872faadaa5ac676677d244f3d5093795cf892
Danny Ellwood <dellwoo2@asiapac.globalcsc.net>
2017-02-24 13:17:38 +0800

    args change

--- timer/timer.go
+++ timer/timer.go
@@ -115,7 +115,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	//key = args[0]
 	//valAsbytes, err := stub.GetState(key)
 	x:=int64(count)
-        str1, err:=strconv.FormatInt(x, 10)
+        str1:=strconv.FormatInt(x,10)
 	valAsbytes:=[]byte(str1)
 	//if err != nil {
 	//	jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
