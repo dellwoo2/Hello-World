f7807620cdabf127b1e1aad1d889dd9897c0e5cc
Danny Ellwood <dellwoo2@asiapac.globalcsc.net>
2017-02-24 12:57:48 +0800

    Timer.go updated

--- timer/timer.go
+++ timer/timer.go
@@ -104,22 +104,22 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 
 // read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
-	var err error
-/*
+	//var key, jsonResp string
+	//var err error
+
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
-	key = args[0]
+	//key = args[0]
 	//valAsbytes, err := stub.GetState(key)
         str1:=strconv.ParseInt(count, 10, 64)
 	valAsbytes:=[]byte(str1)
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
-	}
+	//if err != nil {
+	//	jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+	//	return nil, errors.New(jsonResp)
+	//}
 
-*/
+
 	return valAsbytes, nil
 }
